b218a7a02adce86a05497fd88e3d36a8dd4d5968
root <root@WIN-F4NSTCRMEDC>
2024-09-06 23:46:48 +0700

    .

--- src/Write.php
+++ src/Write.php
@@ -7,7 +7,19 @@ const FILENAME = "log.txt";
 
 function log(string $message): void
 {
-    $logFilePath = DIRECTORY . '/' . FILENAME;
+    $logFilePath = DIRECTORY . "/" . FILENAME;
     $timestamp = date('Y-m-d H:i:s');//временной штамп для отслеживания внесенных данных
     file_put_contents($logFilePath, "[{$timestamp}] {$message}" . PHP_EOL, FILE_APPEND);
+}
+
+function checkAndCreateFile(): void
+{
+    //проверка директории и файла
+    if (!file_exists(DIRECTORY)) {
+        mkdir(DIRECTORY, 0777, true);
+    }
+    $logFilePath = DIRECTORY . "/" . FILENAME;
+    if (!file_exists($logFilePath)) {
+        file_put_contents($logFilePath, "");
+    }
 }
